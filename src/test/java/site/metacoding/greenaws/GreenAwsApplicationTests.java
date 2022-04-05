3d40016aa489ba078cdf378230f69f9cd00687bf
gyung <282203@naver.com>
2022-04-05 17:15:40 +0900

    프로젝트

---
+++ src/test/java/site/metacoding/greenaws/GreenAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.greenaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
